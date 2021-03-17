51024e9d57d849bac40a774d1b3b551d238822c3
fistadev <fabiofistarol@gmail.com>
2021-03-17 12:34:45 +0100

    updates m1_d1
    starting python module

---
+++ M1/D3/m1_d3_deepnote.py
@@ -0,0 +1,24 @@
+# if 2 + 2 == 4:
+#     print(True)
+
+# Function
+def how_r_u(x, r=", thanks"):
+    return x+r
+
+
+print("How are you doing?")
+how_r_u("okay", r)
+
+
+
+l = ['Gabriel', 'Pathi', "nurlan"]
+d = {'Gabriel': 1, 'camelia': 2, 'abishek': 2, 'sven': 2, 'Umidjan':0}
+d['Gabriel']
+
+for key in l:
+    if key in d:
+        d[key]+=1
+    else:
+        d[key]=1
+
+d
