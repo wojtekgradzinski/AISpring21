878761f3ba759cb4e5dbf161e9edc5baf755faa6
fistadev <fabiofistarol@gmail.com>
2021-05-03 11:48:35 +0200

    Update FE1

---
+++ M4 - Feature Engineering/01. Robust ML/class_notes.py
@@ -0,0 +1,25 @@
+# Feature Engineering
+
+# Decision Tree
+"""
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+"""
+
+# Random Forest
+"""
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+"""
+
+# Gradient Boosting
+"""
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from xgboost import XGBClassifier, XGBRegressor
+from lightgbm import LGBClassifier, LGBMRegressor
+from catboost import CatBoostClassifier, CatBoostRegressor
+"""
+
+
+# GBM Hyperparams
+
